c2e6e27706fb3295e2e699108aa4f00e07edb82c
Akash R <akaaash04@gmail.com>
2025-09-02 10:11:24 +0530

    Update DB.java

--- DB.java
+++ DB.java
@@ -3,6 +3,6 @@ import java.sql.*;
 public class DB {
     public static Connection getCon() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "Akash@123");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "your_db_password");
     }
 }
